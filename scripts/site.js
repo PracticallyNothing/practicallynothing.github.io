cfb4ce7d687d1daf127e3be23a2319826bd889ba
Practically Nothing <practicallynothingatall@gmail.com>
2019-12-11 16:51:39 +0200

    Portfolio page is coming along nicely

---
+++ scripts/site.js
@@ -0,0 +1,61 @@
+let menu = document.getElementById("menu");
+let initialMenuHeight = menu.clientHeight;
+let activePage = null
+
+const scrollToTop = () => {
+	const c = document.documentElement.scrollTop || document.body.scrollTop;
+	if (c > 0) {
+		window.requestAnimationFrame(scrollToTop);
+		window.scrollTo(0, c - c / 8);
+	}
+};
+
+function hide() {
+	menu.animate([
+		// keyframes
+		{ transform: 'translateX(-100%)' }, 
+		{ transform: 'translateX(0)' }
+	], { 
+		// timing options
+		duration: 300,
+		easing: "ease-out",
+		iterations: 1, 
+		fill: "forwards"
+	});
+	setTimeout(() => {
+		activePage.classList.add("hidden");
+		activePage=null
+	}, 280);
+	scrollToTop();
+}
+
+function show(section) {
+	menu.animate([
+		// keyframes
+		{ transform: 'translateX(0)' }, 
+		{ transform: 'translateX(-100%)' }
+	], { 
+		// timing options
+		duration: 300,
+		easing: "ease-out",
+		iterations: 1, 
+		fill: "forwards"
+	});
+	let page = document.querySelector(".portfolio-page#" + section.id);
+
+	console.log(".portfolio-page#" + section.id)
+
+	page.classList.remove("hidden");
+	activePage = page;
+
+	scrollToTop();
+}
+
+function init() {
+	let elems = document.getElementsByClassName("button");
+	for(let i = 0; i < elems.length; i++) {
+		elems[i].onclick = () => { show(elems[i]); }
+	}
+}
+
+init();
